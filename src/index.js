fec7ff4400f4147b8cd9218fae6c8e83ca14ad05
AdarshMitr <adarshkumar019@gmail.com>
2023-06-07 11:54:40 +0530

    removing hash router

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 
 import { store } from './features/store';
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
    <React.StrictMode>
-<HashRouter>
+
     <Provider store={store}>
     
     <App />
     
     </Provider>
-    </HashRouter>
+   
   </React.StrictMode>
  
  
